60e48f07df6186795155c8d6fd35fb162f16229a
Tomas Fadrhonc <tomas@fadrhonc.cz>
2019-10-27 17:26:11 +0100

    Update #1

--- themes/new-theme/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>My new app</title>
-</head>
-<body>
-<?php wp_head();?>
